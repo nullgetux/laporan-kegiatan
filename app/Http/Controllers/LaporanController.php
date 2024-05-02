76c56462ee87bc2b2ef6c67445f4cdd56a9ccae9
nullgtx <chupunk3@gmail.com>
2024-05-02 16:59:09 +0700

    #2-export-view-details

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Models\Laporan;
 use App\Http\Requests\LaporanRequest;
+use App\Exports\LaporanExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class LaporanController extends Controller
 {
@@ -19,6 +21,20 @@ class LaporanController extends Controller
     {
         return view ("laporan.create");
     }
+
+    public function show($id)
+    {
+        $laporan = Laporan::find($id);
+
+        if (!$laporan) {
+            return redirect()->back()->with('failed', 'Data Laporan yang akan ditampilkan tidak ditemukan!');
+        }
+
+        $data['laporan'] = $laporan;
+        
+        return view('laporan.show', $data);
+    }
+
 
     public function store(LaporanRequest $request)
     {
@@ -35,4 +51,10 @@ class LaporanController extends Controller
             return redirect(route('laporan'))->with('success', 'Laporan Berhasil Ditambahkan!');
         }
     }
+
+    public function export()
+	{
+		return Excel::download(new LaporanExport, 'laporan-kegiatan.xlsx');
+	}
+
 }
