15b740d71de1f91ccf35b68eb3dfe30159cc8783
nullgtx <chupunk4@gmail.com>
2024-05-04 12:31:32 +0700

    #2-route-delete

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/laporan', [App\Http\Controllers\LaporanController::class, 'index'])->name('laporan');
 Route::get('/laporan/create', [App\Http\Controllers\LaporanController::class, 'create'])->name('laporan.create');
 Route::get('/laporan/show/{id}', [App\Http\Controllers\LaporanController::class, 'show'])->name('laporan.show');
+Route::get('/laporan/delete/{id}', [App\Http\Controllers\LaporanController::class, 'delete'])->name('laporan.delete');
 Route::get('/laporan/show/export/{id}', [App\Http\Controllers\LaporanController::class, 'exportpdf'])->name('laporan.exportpdf');
 Route::post('/laporan', [App\Http\Controllers\LaporanController::class, 'store'])->name('laporan.store');
 Route::get('/laporan/export', [App\Http\Controllers\LaporanController::class, 'export'])->name('laporan.export');
